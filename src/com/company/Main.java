31ef6c77aef8b91bce5ebb570cd49f2afd0df23c
Valery Dub <cosmanatom@gmail.com>
2024-09-25 02:02:57 +0300

    Added output with two languages

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("ПриветМир!");
-    System.out.println("Hello World!");
+	System.out.println("ПриветМир!!!");
+    System.out.println("Hello World!!!");
     }
 }
